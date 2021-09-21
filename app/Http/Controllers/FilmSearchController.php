14593fc66b8ec76913ef12daac67362b0c8dc5d1
zhork <roman@thunder.local>
2021-09-21 14:08:01 +0200

    Routes changed, API only, added search entry point

---
+++ app/Http/Controllers/FilmSearchController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FilmSearchController extends Controller
+{
+    public function __construct() {}
+
+    public function search(Request $request) {}
+}
